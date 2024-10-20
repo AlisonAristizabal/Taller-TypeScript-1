bc16559af744fe0770540046928017446d7fab04
Alison Daiana Aristizabal <a.aristizabalg@uniandes.edu.co>
2024-10-20 17:59:41 -0500

    script

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+import { series } from './data.js';
+const seriesBody = document.getElementById('seriesBody');
+series.forEach((serie) => {
+    const row = seriesBody.insertRow();
+    row.innerHTML = `
+        <td>${serie.id}</td>
+        <td>${serie.name}</td>
+        <td>${serie.channel}</td>
+        <td>${serie.seasons}</td>
+    `;
+});
+// Calcular el promedio de temporadas
+const averageSeasons = series.reduce((sum, serie) => sum + serie.seasons, 0) / series.length;
+// Agregar una fila al final de la tabla con el promedio
+const averageRow = seriesBody.insertRow();
+averageRow.innerHTML = `
+    <td colspan="3"><strong>Promedio de temporadas:</strong></td>
+    <td><strong>${averageSeasons.toFixed(2)}</strong></td>
+`;
